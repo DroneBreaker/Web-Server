c873c4026e6f574682758dde093750e2c800beea
DroneBreaker <brownierz01@gmail.com>
2024-01-23 09:26:35 +0000

    implemented the server and request struct

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,56 @@
 fn main() {
-    println!("Hello, world!");
+    //Craete an enum instance
+    let get = Method::GET;
+    let post = Method::POST;
+    let put = Method::PUT;
+    let delete = Method::DELETE;
+    let head = Method::HEAD;
+    let connect = Method::CONNECT;
+    let options = Method::OPTIONS;
+    let trace = Method::TRACE;
+    let patch = Method::PATCH;
+
+    //Create a variable
+    let server = Server::new("127.0.0.1:8080".to_string());
+    server.run();
+}
+
+struct Server {
+    addr: String, 
+}
+
+//functionality for Struct - Server
+// Self is used to point to the struct Server
+impl Server {
+    fn new(addr: String) -> Self {
+        Self {
+            addr: addr
+        }
+    }
+
+    fn run(self) {
+        println!("Listening on: {}", self.addr)
+    }
+}
+
+struct Request {
+    path: String,
+    query_string: String,
+    method: Method
+}
+
+enum Method {
+    GET,
+    POST, 
+    PUT, 
+    DELETE,
+    HEAD,
+    CONNECT,
+    OPTIONS,
+    TRACE,
+    PATCH
+}
+
+impl Request {
+    
 }
