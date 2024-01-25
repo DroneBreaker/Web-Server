d7a9d1bab65c20976d9ae874ab8a9180ac8b7c2e
DroneBreaker <brownierz01@gmail.com>
2024-01-25 17:00:07 +0000

    Implemented the server and request modules

--- src/http/request.rs
+++ src/http/request.rs
@@ -1,7 +1,35 @@
 use super::method::Method;
+use std::convert::TryFrom;
 
 pub struct Request {
     path: String,
     query_string: Option<String>,
     method: Method
+}
+
+impl Request {
+    //funcion accept a buffer that would be 
+    //a byte slice and return a request
+    fn from_byte_array(buf: &[u8]) -> Result<Self, String> {
+
+    }
+}
+
+impl TryFrom<&[u8]> for Request {
+    type Error = String;
+
+    fn try_from(value: &[u8]) -> Result<Self, Self::Error> {
+        unimplemented!()
+    }
+}
+
+//trait to accwpet a ref to self
+trait Encrypt {
+    fn encrypt(&self) -> Self;
+}
+
+impl Encrypt for String {
+    fn encrypt(&self) -> Self {
+        unimplemented!()
+    }
 }
--- src/server.rs
+++ src/server.rs
@@ -1,4 +1,5 @@
 use std::net::TcpListener;
+use std::io::Read;
 
 //functionality for Struct - Server
 // Self is used to point to the struct Server
@@ -9,7 +10,8 @@ pub struct Server {
 impl Server {
     pub fn new(addr: String) -> Self {
         Self {
-            addr: addr
+            // address is to address
+            addr
         }
     }
 
@@ -20,6 +22,36 @@ impl Server {
 
         let listener = TcpListener::bind(&self.addr).unwrap();
 
-        loop {}
+        loop {
+            // to find a match - based on patterm match
+            match listener.accept() {
+                Ok((mut stream, _)) => {
+                    //read the byte
+                    let mut buffer = [0; 1024];
+                    match stream.read(&mut buffer) {
+                        Ok(_) => {
+                            println!("Received a request: {}", String::from_utf8_lossy(&buffer))
+                        }
+
+                        Err(e) => println!("Failed to read from conection: {}", e)
+                    }
+                }
+
+                Err(e) => println!("Failed to establiesh a connection: {}", e)
+            }
+
+            // let result = listener.accept();
+
+            // //  check to see if result is an error
+            // //  and use the unwrap function
+            // if result.is_err() {
+            //     continue;
+            // } 
+
+            // let stream = result.unwrap();
+        }
+
+        //custom tuple
+        // let tup = (5, "a", listener);
     }
 }
